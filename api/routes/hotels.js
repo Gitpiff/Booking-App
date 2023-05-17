1875443934bce5c8f3c9db080e897508d8e1be0d
Victor <viknavarro@gmail.com>
2023-05-18 00:55:43 +0100

    Hotel CRUD operations working

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -27,4 +27,39 @@ router.put("/:id", async (req, res) => {
     }
 })
 
+//Delete
+router.delete("/:id", async (req, res) => {
+
+    try {                                                   //id
+        const hotel = await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Hotel has been deleted")
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//Get Specific Hotel
+router.get("/:id", async (req, res) => {
+
+    try {
+        const hotel = await Hotel.findById(req.params.id)
+        res.status(200).json(hotel)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//Get All Hotels
+router.get("/", async (req, res) => {
+
+    try {
+        const hotels = await Hotel.find()
+        res.status(200).json(hotels)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
 export default router
